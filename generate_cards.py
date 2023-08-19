01b67222c29b45d2f002ea8f47149af210d1fd07
Martin Häcker <mhaecker@mac.com>
2023-08-19 18:46:50 +0200

    Get started generating cards.

---
+++ generate_cards.py
@@ -0,0 +1,14 @@
+from jinja2 import Environment, PackageLoader, select_autoescape
+from yaml import load, Loader
+
+env = Environment(
+    loader=PackageLoader("generate_cards"),
+    autoescape=select_autoescape()
+)
+template = env.get_template("cards.html.j2")
+
+with open('cards.yml') as yaml_file:
+    data = load(yaml_file, Loader=Loader)
+
+with open('cards.html', 'w') as html_file:
+    html_file.write(template.render(data))
